e6b42ac98f5f0af434d00b2748f5dd12da4a21b7
Josh Williams <josh@jodawill.com>
2021-06-07 17:03:41 -0400

    add 1 exception and remove commented code

--- prime.py
+++ prime.py
@@ -1,13 +1,8 @@
 from math import ceil, sqrt
 
 def isPrime(n):
+  n == 1 and return False
   for i in range(2, ceil(sqrt(n))):
     if n % i == 0:
       return False
   return True
-
-#n = int(input("Enter a number: "))
-#if isPrime(n):
-#  print(f'{n} is prime')
-#else:
-#  print(f'{n} is not prime')
