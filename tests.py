fb0988cec97e773bf6773a70b48276cf2feb115e
Josh Williams <josh@jodawill.com>
2021-06-07 16:59:16 -0400

    use exit codes in test

--- tests.py
+++ tests.py
@@ -1,11 +1,16 @@
 from prime import *
+from sys import exit
 
 def test(f, expect, *args):
   if f(*args) == expect:
     print(f'TEST: {f.__name__}{args} -- PASS')
+    return 0
   else:
     print(f'TEST: {f.__name__}{args} -- FAIL')
+    return 1
 
-test(isPrime, True, 17)
-test(isPrime, False, 24)
-test(isPrime, False, 1)
+exit_code = 0
+exit_code |= test(isPrime, True, 17)
+exit_code |= test(isPrime, False, 24)
+exit_code |= test(isPrime, False, 1)
+exit(exit_code)
