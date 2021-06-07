5696dc08226be2f7e9470808a8e4495c1f23e2d9
Josh Williams <josh@jodawill.com>
2021-06-07 17:08:31 -0400

    fix syntax error

--- prime.py
+++ prime.py
@@ -1,7 +1,8 @@
 from math import ceil, sqrt
 
 def isPrime(n):
-  n == 1 and return False
+  if n == 1:
+    return False
   for i in range(2, ceil(sqrt(n))):
     if n % i == 0:
       return False
